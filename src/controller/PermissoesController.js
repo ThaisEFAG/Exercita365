629d8533947e753b79039c80da586c72085bf9a2
Thaís <thaisefag@gmail.com>
2024-08-02 07:10:59 -0300

    controller_permissoes_int

--- src/controller/PermissoesController.js
+++ src/controller/PermissoesController.js
@@ -2,17 +2,27 @@ const Permissoes = require("../models/Permissoes");
 const Usuario = require("../models/Usuario");
 
 class PermissoesController {
-    async cadastrar(request, response){
-        try{
-            const descricao = request.body;
+  async cadastrar(request, response) {
+    try {
+      const descricao = request.body;
 
-            const permissao = await Permissoes.create(descricao)
-            response(201).json(permissao)
-        }catch(error){
-            console.log(error);
-            response(500).json({mensagem: "Houve um erro ao cadastrar permissao"})
-        }
+      const permissao = await Permissoes.create(descricao);
+      response(201).json(permissao);
+    } catch (error) {
+      console.log(error);
+      response(500).json({ mensagem: "Houve um erro ao cadastrar permissao" });
+    }
+  }
+
+  async deletar(request, response) {
+    const id = request.params.id;
+    const permissao = await Permissoes.findByPk(id);
+
+    if (!permissao) {
+      response.status(404).json("Permissao não encontrada");
     }
 
-    async deletar(request, response)
+    await permissao.destroy();
+    response.status(204).json({ mensagem: "Permissao deletada com sucesso" });
+  }
 }
