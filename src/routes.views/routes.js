0081cde62c23335d4a7a075e41b726801b122ca8
Thaís <thaisefag@gmail.com>
2024-08-03 13:04:27 -0300

    show

--- src/routes.views/routes.js
+++ src/routes.views/routes.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const usuariosRoutes = require("./usuarios.routes");
 const loginRoutes = require("./login.routes");
 const locaisRoutes = require("./locais.routes");
+const permissoesRoutes = require("./permissoes.routes");
 // const validaToken = require("../middlewares/validaToken");
 
 const routes = new Router();
@@ -9,5 +10,6 @@ const routes = new Router();
 routes.use("/usuarios", usuariosRoutes);
 routes.use("/login", loginRoutes);
 routes.use("/locais", locaisRoutes);
+routes.use("/", permissoesRoutes);
 
 module.exports = routes;
