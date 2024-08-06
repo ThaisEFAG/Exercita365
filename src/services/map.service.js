1a7405dc0abe0963765ac16cc56beb8e279fc153
Thaís <thaisefag@gmail.com>
2024-08-05 21:21:47 -0300

    permissoes

--- src/services/map.service.js
+++ src/services/map.service.js
@@ -1,9 +1,32 @@
+// google.com/maps?q=-nºlatitude,-nºlongitude -- nominatim, opencep, cep.awesomeapi.com.br/json/88653000('https://cep.awesomeapi.com.br/json/')
+
 const axios = require("axios");
 const linkMapApi =
   "https://nominatim.openstreetmap.org/search?format=json&country=Brazil&limit=1";
 
 async function getMapLocal(cep) {
-  // try{
-  // const response = await axios.getAdapter(`${linkMapApi}&postalcode=${cep}`)
-  // }
+  try {
+    const response = await axios.getAdapter(`${linkMapApi}&postalcode=${cep}`);
+
+    if (!response.data) {
+      throw new Error("Erro: CEP não encontrado");
+    }
+    const { lat, lon, display_name } = response.data[0];
+
+    return { lat, lon, display_name };
+  } catch (error) {
+    return { erro: "Erro API CEP" };
+  }
+}
+
+async function getGoogleMapsLink(coordenadas) {
+  try {
+    const { lat, lon } = coordenadas;
+
+    const googleMapsLink = `https://www.google.com/maps?q=${lat}, ${lon}`;
+
+    return googleMapsLink;
+  } catch (error) {
+    throw new Error("Erro ao gerar o link do Google Maps");
+  }
 }
